328140a5cd1345bdc27fcc0f78ca8cba98fb5c9b
huy-burN <nhathuy200712@gmail.com>
2025-03-25 23:44:23 +0700

    load dataset

---
+++ src/model2.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+# Load dataset

d993ccc3009b7ff2b028344a48862ea40b125d29
huy-burN <nhathuy200712@gmail.com>
2025-08-20 23:44:47 +1000

    ela13

--- src/data_loader.py
+++ src/data_loader.py
@@ -17,7 +17,7 @@ def load_data(filepath, text_column, label_column):
     # Load the CSV file into a DataFramee
     data = pd.read_csv(filepath)
     
-    # Extract the text and label
+    # Extract the text and labels from the DataFrame
     X = data[text_column].astype(str).tolist()
     y = data[label_column].astype(int).tolist()
     

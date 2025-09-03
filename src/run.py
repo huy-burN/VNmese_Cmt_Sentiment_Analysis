5771f5dda13e1c27d9504aba665b630fb5d675a2
huy-burN <nhathuy200712@gmail.com>
2025-09-04 00:18:26 +1000

    ctemmean123

--- src/run.py
+++ src/run.py
@@ -2,7 +2,7 @@ import argparse
 from src.data_loader import load_data
 from src.model import SentimentAnalysisModel
 from src.utils import preprocess_text
-#miiitinh minhf vuot nganhua
+#miiitinh minhf vuot nganhuadoi bó e
 def main():
     parser = argparse.ArgumentParser(description="Vietnamese Comment Sentiment Analysis")
     parser.add_argument('--data', type=str, required=True, help="Path to the dataset")
